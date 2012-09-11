d9e9ff80bccaa3ad164300d0548935a83045634d
David Nelson <davcnelson25@gmail.com>
2012-09-10 20:23:44 -0700

    initial project happy trials

---
+++ dummy1/src/com/dave/practice/PracticeMain.java
@@ -0,0 +1,15 @@
+package com.dave.practice;
+
+public class PracticeMain {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hi Avery!");
+		System.out.println("Hi Trevor!");
+		System.out.println("Hi Mommy!");
+		System.out.println("Hi Auntie!");
+		System.out.println("Hi Daddy!");
+		System.out.println("Goodnight Avery!");
+		System.out.println("I love you Avery!");
+	}
+}
